5c6068cdad1fa1f30d9e6e76af392f5eb3c43b6a
Gerson0409 <54654011+Gerson0409@users.noreply.github.com>
2020-06-05 21:17:56 -0600

    Clase 28-05-20

---
+++ holamundo/src/main/java/com/gerson/holamundo/holamundo/Controlador.java
@@ -0,0 +1,18 @@
+package com.gerson.holamundo.holamundo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+
+public class Controlador {
+
+	@GetMapping("/")
+	public String Inicio(Model modelo) {
+		String mensaje = "Hola desde el controlador";
+		modelo.addAttribute("saludo", mensaje);
+		return "Index";
+	}
+}
---
+++ holamundo/src/test/java/com/gerson/holamundo/holamundo/HolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerson.holamundo.holamundo;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
